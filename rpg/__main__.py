a53aa22a8bf4cc3119236d538c1c2291cf2da39b
DragonMoffon <happydog.aja@gmail.com>
2025-12-18 20:55:08 +1300

    tested multi fighters

--- rpg/__main__.py
+++ rpg/__main__.py
@@ -3,4 +3,4 @@ import rpg.content as content
 from rpg.console import Fight
 
 if __name__ == "__main__":
-    Fight((content.Characters.CS,), (content.Characters.COP,), show_debug=False).run()
+    Fight((content.Characters.CS, content.Characters.CS, content.Characters.CS,), (content.Characters.COP, content.Characters.COP, content.Characters.COP,), show_debug=False).run()
